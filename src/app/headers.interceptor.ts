728e11e0def717cd9d3fc2a35230d2f695946981
iailyas <62544436+iailyas@users.noreply.github.com>
2023-05-16 13:10:17 +0300

    add new functionality

--- src/app/headers.interceptor.ts
+++ src/app/headers.interceptor.ts
@@ -3,15 +3,18 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HttpErrorResponse
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { JwtService } from './services/jwt.service';
+import { Router } from '@angular/router';
+
 
 @Injectable()
 export class HeadersInterceptor implements HttpInterceptor {
 
-  constructor(private jwt: JwtService) { }
+  constructor(private jwt: JwtService, private router: Router) { }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     const apiKey = 'Bearer ' + this.jwt.token;
@@ -20,6 +23,14 @@ export class HeadersInterceptor implements HttpInterceptor {
         'Authorization': apiKey,
       }
     })
-    return next.handle(request);
+    return next.handle(request).pipe(tap(() => { },
+      (err: any) => {
+        if (err instanceof HttpErrorResponse) {
+          if (err.status !== 401) {
+            return;
+          }
+          this.router.navigate(['AddUser']);
+        }
+      }));
   }
 }
